5db9dd51d0ccc243ef1293af6f2506b62378f7ab
arsenal <strigunovqa@gmail.com>
2018-05-31 20:43:02 -0700

    Wrapped base test unput into properties file

--- src/test/java/Utils/BaseTest.java
+++ src/test/java/Utils/BaseTest.java
@@ -12,20 +12,32 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class BaseTest {
 
     protected static WebDriver driver;
     protected static org.apache.logging.log4j.Logger logger = LogManager.getLogger(Tests.TeamModuleTests.class);
+    protected static Properties prop = new Properties();
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/base.properties");
+        prop.load(file);
+    }
 
     @BeforeMethod(groups = "team")
     public void navigateToBranchViaGoogleSearchAndOpenTeamPage() {
-        System.setProperty("webdriver.chrome.driver", "/Users/arsenal/webdrivers/chromedriver");
+        System.setProperty(prop.getProperty("browserDriver"), prop.getProperty("pathToBrowserDriver"));
         driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get(prop.getProperty("googleURL"));
         maximizeWindow();
         GooglePage googlePage = new GooglePage();
-        googlePage.changeSearchQuery("branch metrics");
+        googlePage.changeSearchQuery(prop.getProperty("queryBranchMetrics"));
         GoogleResultsPage googleResultsPage = googlePage.hitEnterInSearchBar();
         googleResultsPage.clickBranchLink();
         BranchPage branchPage = new BranchPage();
